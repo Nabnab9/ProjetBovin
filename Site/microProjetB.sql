22b5633dffa6c6c082ea69937fd0fe44e62a1952
Nabnab <alban.rousseau9@gmail.com>
2015-12-07 00:10:01 +0100

    modif bdd

--- Site/microProjetB.sql
+++ Site/microProjetB.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.13
+-- version 4.1.14
 -- http://www.phpmyadmin.net
 --
--- Client :  localhost
--- Généré le :  Lun 23 Novembre 2015 à 13:16
--- Version du serveur :  5.5.37-MariaDB
--- Version de PHP :  5.5.19
+-- Client :  127.0.0.1
+-- Généré le :  Lun 07 Décembre 2015 à 00:09
+-- Version du serveur :  5.6.17
+-- Version de PHP :  5.5.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Base de données :  `s122613`
+-- Base de données :  `projet bovin`
 --
 
 -- --------------------------------------------------------
@@ -27,17 +27,11 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `abattoir` (
-  `nom_abat` varchar(50) NOT NULL,
-  `n_tel_abat` text NOT NULL,
-  `adr_abat` text NOT NULL
+  `nom_abat` varchar(30) NOT NULL,
+  `n_tel_abat` varchar(10) NOT NULL,
+  `adr_abat` text NOT NULL,
+  PRIMARY KEY (`nom_abat`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Contenu de la table `abattoir`
---
-
-INSERT INTO `abattoir` (`nom_abat`, `n_tel_abat`, `adr_abat`) VALUES
-('la meule', '0215659764', 'osef');
 
 -- --------------------------------------------------------
 
@@ -47,10 +41,11 @@ INSERT INTO `abattoir` (`nom_abat`, `n_tel_abat`, `adr_abat`) VALUES
 
 CREATE TABLE IF NOT EXISTS `agriculteur` (
   `id_agriculteur` int(11) NOT NULL,
-  `nom_agri` text NOT NULL,
-  `prenom_agri` text NOT NULL,
-  `n_tel_agri` text NOT NULL,
-  `adr_agri` text NOT NULL
+  `nom_agri` varchar(30) NOT NULL,
+  `prenom_agri` varchar(30) NOT NULL,
+  `n_tel_agri` varchar(10) NOT NULL,
+  `adr_agri` text NOT NULL,
+  PRIMARY KEY (`id_agriculteur`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -58,7 +53,10 @@ CREATE TABLE IF NOT EXISTS `agriculteur` (
 --
 
 INSERT INTO `agriculteur` (`id_agriculteur`, `nom_agri`, `prenom_agri`, `n_tel_agri`, `adr_agri`) VALUES
-(13, 'rabbit', 'roger', '02156549764', 'osef');
+(0, '', '', '', ''),
+(15, 'rousse', 'quatrre', '0634263535', 'ruebarbe'),
+(1234, 'cinq', 'cinq', '0634263535', 'ruebarbe'),
+(1235, 'cinq', 'cinq', '0634263535', 'ruebarbe');
 
 -- --------------------------------------------------------
 
@@ -67,58 +65,28 @@ INSERT INTO `agriculteur` (`id_agriculteur`, `nom_agri`, `prenom_agri`, `n_tel_a
 --
 
 CREATE TABLE IF NOT EXISTS `bovin` (
-  `code_boucle` varchar(12) NOT NULL,
-  `nom_bovin` text NOT NULL,
-  `pays` text NOT NULL,
-  `departement` int(11) NOT NULL,
-  `exploitation` text NOT NULL,
-  `num_travail` text NOT NULL,
+  `code_boucle` varchar(14) NOT NULL,
+  `nom_bovin` varchar(30) NOT NULL,
+  `pays` varchar(2) NOT NULL,
+  `departement` int(2) NOT NULL,
+  `num_travail` int(4) NOT NULL,
   `date_naissance` date NOT NULL,
-  `date_mort` date NOT NULL,
-  `race` text NOT NULL,
-  `sexe` text NOT NULL,
-  `id_agriculteur_bovin` int(11) NOT NULL,
-  `nom_abat_bovin` text NOT NULL
+  `date_mort` date DEFAULT NULL,
+  `race` varchar(30) NOT NULL,
+  `sexe` char(1) DEFAULT NULL,
+  `id_agriculteur_bovin` int(4) NOT NULL,
+  `nom_abat_bovin` varchar(30) DEFAULT 'BovinStillAlive',
+  PRIMARY KEY (`code_boucle`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `bovin`
 --
 
-INSERT INTO `bovin` (`code_boucle`, `nom_bovin`, `pays`, `departement`, `exploitation`, `num_travail`, `date_naissance`, `date_mort`, `race`, `sexe`, `id_agriculteur_bovin`, `nom_abat_bovin`) VALUES
-('555', 'Thierry', 'France', 72, 'osef', '12', '2011-12-12', '2012-12-12', 'Francaise', 'oui', 13, 'la meule');
-
---
--- Index pour les tables exportées
---
-
---
--- Index pour la table `abattoir`
---
-ALTER TABLE `abattoir`
- ADD PRIMARY KEY (`nom_abat`);
-
---
--- Index pour la table `agriculteur`
---
-ALTER TABLE `agriculteur`
- ADD PRIMARY KEY (`id_agriculteur`);
-
---
--- Index pour la table `bovin`
---
-ALTER TABLE `bovin`
- ADD PRIMARY KEY (`code_boucle`), ADD KEY `ETRANGERE` (`id_agriculteur_bovin`);
-
---
--- Contraintes pour les tables exportées
---
-
---
--- Contraintes pour la table `bovin`
---
-ALTER TABLE `bovin`
-ADD CONSTRAINT `contrainte_id_agri` FOREIGN KEY (`id_agriculteur_bovin`) REFERENCES `agriculteur` (`id_agriculteur`) ON DELETE NO ACTION;
+INSERT INTO `bovin` (`code_boucle`, `nom_bovin`, `pays`, `departement`, `num_travail`, `date_naissance`, `date_mort`, `race`, `sexe`, `id_agriculteur_bovin`, `nom_abat_bovin`) VALUES
+('DE 78 1235 454', 'test2', 'DE', 78, 4545, '2012-01-06', '0000-00-00', 'Corse', 'M', 1235, ''),
+('Fr 72 1234 567', 'Nom du bovin', 'Fr', 72, 5678, '2015-12-12', '0000-00-00', 'Saosnoise', 'F', 1234, ''),
+('FR 72 1235 123', 'test', 'FR', 72, 1237, '2012-01-13', '0000-00-00', 'Rouge des prés', 'F', 1235, '');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
