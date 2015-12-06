96011d8e28fc44df10b06e8aeed533a23bbbcf60
Nabnab <alban.rousseau9@gmail.com>
2015-12-06 21:33:06 +0100

    modif fiche vache

--- Site/verif.php
+++ Site/verif.php
@@ -19,16 +19,16 @@
 				//le bovin
 				$code_boucle= $_POST['code_boucle'];
 				$nom_bovin= $_POST['nom_bovin'];
-				//$pays= $_POST['pays'];
-				//$departement= $_POST['departement'];
-				//$exploitation= $_POST['exploitation'];
-				//$num_travail= $_POST['num_travail'];
+				$pays= $_POST['pays'];
+				$departement= $_POST['departement'];
+				$exploitation= $_POST['exploitation'];
+				$num_travail= $_POST['num_travail'];
 				$date_naissance= $_POST['date_naissance'];
-				//$date_mort= $_POST['date_mort'];
+				$date_mort= $_POST['date_mort'];
 				$race= $_POST['race'];
 				$sexe= $_POST['sexe'];
 				$id_agriculteur_bovin = $_POST['id_agriculteur_bovin'];
-				//$nom_abat_bovin = $_POST['nom_abat_bovin'];
+				$nom_abat_bovin = $_POST['nom_abat_bovin'];
 				
 				//l'agriculteur
 				$id_agriculteur = $_POST['id_agriculteur'];
@@ -37,11 +37,11 @@
 				$n_tel_agri = $_POST['n_tel_agri'];
 				$adr_agri = $_POST['adr_agri'];
 				
-				/*//l'abattoir
+				//l'abattoir
 				$nom_abat = $_POST['nom_abat'];
 				$n_tel_abat = $_POST['n_tel_abat'];
 				$adr_abat = $_POST['adr_abat'];
-				*/
+				
 				
 				
 				
